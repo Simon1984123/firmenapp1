4f1a8113bbd01839ad3eb62a131901dd92803e72
Simon1984123 <slaux@laux-gmbh.de>
2025-06-18 01:25:02 +0200

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const Home = () => <h1>Willkommen in der Firmenapp</h1>;
+export default Home;
---
+++ src/components/ui/button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+export const Button = ({ children, onClick }) => (
+  <button onClick={onClick} className="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">
+    {children}
+  </button>
+);
